103c37d84f2ac316245ae9db8546fc06dd42e62d
LUCAS EDUARDO FERREIRA DOS SANTOS <lucas.efsantos@senacsp.edu.br>
2020-01-21 15:08:29 -0300

    Segundo commit

--- SncMusic/Banco.cs
+++ SncMusic/Banco.cs
@@ -10,13 +10,21 @@ namespace SncMusic
 {
     public static class Banco
     {
+        static MySqlConnection cn;
         public static MySqlCommand Abrir()
         {
-            MySqlConnection cn = new MySqlConnection(Properties.Settings.Default.StrConn);
+            cn = new MySqlConnection(Properties.Settings.Default.StrConn);
             cn.Open();
             MySqlCommand comm = new MySqlCommand();
             comm.Connection = cn;
             return comm;
+        }
+        public static void Fechar()
+        {
+            /// se o estado da conexao for igual a aberto então fechar conexao
+            if (cn.State == System.Data.ConnectionState.Open)
+            { // não é necessário utilizar chaves em estrurturas caso tenha apenas um comando}
+                cn.Close();
         }
     }
 
